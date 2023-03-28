5334d4b4ef291a771aaa5a36b4d90fc1a5349c88
HeinerAlejandro <heiner.enis@gmail.com>
2023-03-28 00:48:48 -0300

    feat: Add use cases

---
+++ api/cli.py
@@ -0,0 +1,20 @@
+import click
+from core import core_obj
+
+
+@click.group()
+def service_cli():
+    ...
+
+
+@service_cli.command()
+def service_start():
+    core_obj.socket_instance.send(
+        {"hola": "te quiero"},
+        str,
+        lambda data: data["hola"]
+    )
+
+
+if __name__ == '__main__':
+    service_cli()
