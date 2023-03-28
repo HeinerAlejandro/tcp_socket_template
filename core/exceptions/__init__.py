1cfe62bd5a43ecfdaca4eeb7ec1debcac1f53356
HeinerAlejandro <heiner.enis@gmail.com>
2023-03-28 10:20:27 -0300

    feat: Add Core exceptions

---
+++ core/exceptions/__init__.py
@@ -0,0 +1,14 @@
+class ConnectionErrorException(Exception):
+    pass
+
+
+class DataSendingErrorException(Exception):
+    pass
+
+
+class DataRecvErrorException(Exception):
+    pass
+
+
+class SocketNotConnected(Exception):
+    pass
