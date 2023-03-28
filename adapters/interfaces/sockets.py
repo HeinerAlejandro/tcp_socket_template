20375d5ab4bc943eadc285fac092143fa267dd7b
HeinerAlejandro <heiner.enis@gmail.com>
2023-03-28 10:22:00 -0300

    feat: Add Client Adapters

---
+++ adapters/interfaces/sockets.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+import socket
+from typing import Any
+from dataclasses import field
+from ipaddress import (
+    IPv4Address,
+    IPv6Address
+)
+
+
+class SocketClientAbstract(ABC):
+
+    @abstractmethod
+    def connect(self):
+        ...
+
+    @abstractmethod
+    def close(self):
+        ...
+
+    @abstractmethod
+    def send(
+        self,
+        http_options: dict,
+        url_type: str,
+    ) -> int:
+        ...
+
+    @abstractmethod
+    def recv(self, until_complete: bool):
+        ...
+
+
+__all__ = [
+    "SocketClientAbstract"
+]
